e5db8a87c9a334a4e5e893a3af1df541da9f2ac2
Hero <78842166+Heropowerbrine@users.noreply.github.com>
2024-05-20 15:38:49 +0800

    Update MobileData.hx

--- source/mobile/backend/MobileData.hx
+++ source/mobile/backend/MobileData.hx
@@ -2,46 +2,56 @@ package mobile.backend;
 
 import haxe.ds.Map;
 import tjson.TJSON as Json;
+import sys.FileSystem;
+import sys.io.File;
 import haxe.io.Path;
-import openfl.utils.Assets;
 
 class MobileData
 {
 	public static var actionModes:Map<String, VirtualPadButtonsData> = new Map();
 	public static var dpadModes:Map<String, VirtualPadButtonsData> = new Map();
 	public static var extraActions:Map<String, ExtraActions> = new Map();
+	public static var virtualPadConfig:VirtualPadData = {image: 'virtualpad', alpha: ClientPrefs.data.controlsAlpha};
 
 	public static function init()
 	{
-		readDirectory(Paths.getSharedPath('mobile/DPadModes'), dpadModes);
-		readDirectory(Paths.getSharedPath('mobile/ActionModes'), actionModes);
-		#if MODS_ALLOWED
-		for (folder in Mods.directoriesWithFile(Paths.getSharedPath(), 'mobile/'))
+		if (FileSystem.exists(Paths.getSharedPath('mobile/DPadModes')))
 		{
-			readDirectory(Path.join([folder, 'DPadModes']), dpadModes);
-			readDirectory(Path.join([folder, 'ActionModes']), actionModes);
+			for (file in FileSystem.readDirectory(Paths.getSharedPath('mobile/DPadModes')))
+			{
+				if (Path.extension(file) == 'json')
+				{
+					var str = File.getContent(Paths.getSharedPath(Path.join(['mobile/DPadModes/', Path.withoutDirectory(file)])));
+					var json:VirtualPadButtonsData = cast Json.parse(str);
+					dpadModes.set(Path.withoutDirectory(Path.withoutExtension(file)), json);
+				}
+			}
 		}
-		#end
+
+		if (FileSystem.exists(Paths.getSharedPath('mobile/ActionModes')))
+		{
+			for (file in FileSystem.readDirectory(Paths.getSharedPath('mobile/ActionModes')))
+			{
+				if (Path.extension(file) == 'json')
+				{
+					var str = File.getContent(Paths.getSharedPath(Path.join(['mobile/ActionModes/', Path.withoutDirectory(file)])));
+					var json:VirtualPadButtonsData = cast Json.parse(str);
+					actionModes.set(Path.withoutDirectory(Path.withoutExtension(file)), json);
+				}
+			}
+		}
+
+		if (FileSystem.exists(Paths.getSharedPath('data/mobile/config.json')))
+		{
+			var json:VirtualPadData = cast Json.parse(File.getContent(Paths.getSharedPath('mobile/config.json')));
+			if (json.image != null && json.image.length > 0)
+				virtualPadConfig.image = json.image;
+			if (json.alpha != null)
+				virtualPadConfig.alpha = json.alpha;
+		}
 
 		for (data in ExtraActions.createAll())
 			extraActions.set(data.getName(), data);
-	}
-
-	public static function readDirectory(folder:String, map:Dynamic)
-	{
-		#if MODS_ALLOWED if(FileSystem.exists(folder)) #end
-			for (file in Paths.readDirectory(folder))
-			{
-				var fileWithNoLib:String = file.contains(':') ? file.split(':')[1] : file;
-				if (Path.extension(fileWithNoLib) == 'json')
-				{
-				 	#if MODS_ALLOWED file = Path.join([folder, Path.withoutDirectory(file)]); #end
-					var str = #if MODS_ALLOWED File.getContent(file) #else Assets.getText(file) #end;
-					var json:VirtualPadButtonsData = cast Json.parse(str);
-					var mapKey:String = Path.withoutDirectory(Path.withoutExtension(fileWithNoLib));
-					map.set(mapKey, json);
-				}
-			}
 	}
 }
 
@@ -56,7 +66,15 @@ typedef ButtonsData =
 	graphic:String, // the graphic of the button, usually can be located in the VirtualPad xml .
 	x:Float, // the button's X position on screen.
 	y:Float, // the button's Y position on screen.
+	width:Int, // the button's width, default value is 132.
+	height:Int, // the button's height, default value is 127.
 	color:String // the button color, default color is white.
+}
+
+typedef VirtualPadData =
+{
+	image:String,
+	?alpha:Float
 }
 
 enum ExtraActions
