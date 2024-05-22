60667a3bac69709fe560ddcde27c203c62daa8d4
Hero <78842166+Heropowerbrine@users.noreply.github.com>
2024-05-22 17:54:29 +0800

    Wdym u can't find MobileControlSelectSubstate
    I literally just added it

--- source/options/OptionsState.hx
+++ source/options/OptionsState.hx
@@ -86,20 +86,6 @@ class OptionsState extends MusicBeatState
 
 		addVirtualPad('UP_DOWN', 'A_B_C');
 
-		#if (target.threaded)
-		Thread.create(()->{
-			mutex.acquire();
-
-			for (i in VisualsUISubState.pauseMusics)
-			{
-				if (i.toLowerCase() != "none")
-					Paths.music(Paths.formatToSongPath(i));
-			}
-
-			mutex.release();
-		});
-		#end
-
 		super.create();
 	}
 
