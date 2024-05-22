06a2968ada1ce41649a6ac65ae45beb0f6e20238
Hero <78842166+Heropowerbrine@users.noreply.github.com>
2024-05-22 22:48:43 +0800

    Time to begin the revert process
    What I'm going to do now is to try and add normal default mobile controls in first so that it doesn't break again when I try to use the force extra key hitbox method for extra key songs
    (Let's hope this works)

--- source/mobile/flixel/FlxHitbox.hx
+++ source/mobile/flixel/FlxHitbox.hx
@@ -10,7 +10,6 @@ import openfl.display.Shape;
  * It's really easy to customize the layout.
  *
  * @author: Mihai Alexandru, Karim Akra & Lily (mcagabe19)
- * LOL IT'S NO LONGER JUST 4 HINTS, EXTRA KEYS IS HERE -Heropowerbrine
  */
 class FlxHitbox extends FlxMobileInputManager
 {
@@ -21,11 +20,6 @@ class FlxHitbox extends FlxMobileInputManager
 	public var buttonDown:FlxButton = new FlxButton(0, 0, [FlxMobileInputID.hitboxDOWN, FlxMobileInputID.noteDOWN]);
 	public var buttonUp:FlxButton = new FlxButton(0, 0, [FlxMobileInputID.hitboxUP, FlxMobileInputID.noteUP]);
 	public var buttonRight:FlxButton = new FlxButton(0, 0, [FlxMobileInputID.hitboxRIGHT, FlxMobileInputID.noteRIGHT]);
-	public var button5:FlxButton = new FlxButton(0, 0, [FlxMobileInputID.note5]);
-	public var button6:FlxButton = new FlxButton(0, 0, [FlxMobileInputID.note6]);
-	public var button7:FlxButton = new FlxButton(0, 0, [FlxMobileInputID.note7]);
-	public var button8:FlxButton = new FlxButton(0, 0, [FlxMobileInputID.note8]);
-	public var button9:FlxButton = new FlxButton(0, 0, [FlxMobileInputID.note9]);
 	public var buttonExtra:FlxButton = new FlxButton(0, 0);
 	public var buttonExtra2:FlxButton = new FlxButton(0, 0);
 
@@ -66,55 +60,6 @@ class FlxHitbox extends FlxMobileInputManager
 					0xFFF9393F));
 				add(buttonExtra2 = createHint(Std.int(FlxG.width / 2), offsetFir, Std.int(FlxG.width / 2), Std.int(FlxG.height / 4), 0xA6FF00));
 				add(buttonExtra = createHint(0, offsetFir, Std.int(FlxG.width / 2), Std.int(FlxG.height / 4), 0xFF0066FF));
-			case ONE:
-				add(buttonUp = createHint(0, 0, Std.int(FlxG.width), FlxG.height, 0xFFC24B99));
-			case TWO:
-				add(buttonLeft = createHint(0, 0, Std.int(FlxG.width / 2), FlxG.height, 0xFFC24B99));
-				add(buttonRight = createHint(FlxG.width / 2, 0, Std.int(FlxG.width / 2), FlxG.height, 0xFF00FFFF));
-			case THREE:
-				add(buttonLeft = createHint(0, 0, Std.int(FlxG.width / 3), FlxG.height, 0xFFC24B99));
-				add(buttonUp = createHint(426, 0, Std.int(FlxG.width / 3), FlxG.height, 0xFF00FFFF));
-				add(buttonRight = createHint(852, 0, Std.int(FlxG.width / 3), FlxG.height, 0xFF12FA05));
-			case FIVE:
-				add(buttonLeft = createHint(0, 0, Std.int(FlxG.width / 5), FlxG.height, 0xFFC24B99));
-				add(buttonDown = createHint(FlxG.width / 5, 0, Std.int(FlxG.width / 5), FlxG.height, 0xFF00FFFF));
-				add(button5 = createHint(FlxG.width / 2.5, 0, Std.int(FlxG.width / 5), FlxG.height, 0xFFC24B99));
-				add(buttonUp = createHint(768, 0, Std.int(FlxG.width / 5), FlxG.height, 0xFF12FA05));
-				add(buttonRight = createHint(1024, 0, Std.int(FlxG.width / 5), FlxG.height, 0xFFF9393F));
-			case SIX:
-				add(buttonLeft = createHint(0, 0, Std.int(FlxG.width / 6), FlxG.height, 0xFFC24B99));
-				add(buttonDown = createHint(213, 0, Std.int(FlxG.width / 6), FlxG.height, 0xFF00FFFF));
-				add(button5 = createHint(426, 0, Std.int(FlxG.width / 6), FlxG.height, 0xFFC24B99));
-				add(button6 = createHint(639, 0, Std.int(FlxG.width / 6), FlxG.height, 0xFF00FFFF));
-				add(buttonUp = createHint(852, 0, Std.int(FlxG.width / 6), FlxG.height, 0xFF12FA05));
-				add(buttonRight = createHint(1065, 0, Std.int(FlxG.width / 6), FlxG.height, 0xFFF9393F));
-			case SEVEN:
-				add(buttonLeft = createHint(0, 0, Std.int(FlxG.width / 7), FlxG.height, 0xFFC24B99));
-				add(buttonDown = createHint(182, 0, Std.int(FlxG.width / 7), FlxG.height, 0xFF00FFFF));
-				add(button5 = createHint(364, 0, Std.int(FlxG.width / 7), FlxG.height, 0xFFC24B99));
-				add(button6 = createHint(546, 0, Std.int(FlxG.width / 7), FlxG.height, 0xFF00FFFF));
-				add(button7 = createHint(728, 0, Std.int(FlxG.width / 7), FlxG.height, 0xFF12FA05));
-				add(buttonUp = createHint(910, 0, Std.int(FlxG.width / 7), FlxG.height, 0xFF12FA05));
-				add(buttonRight = createHint(1092, 0, Std.int(FlxG.width / 7), FlxG.height, 0xFFF9393F));
-			case EIGHT:
-				add(buttonLeft = createHint(0, 0, Std.int(FlxG.width / 8), FlxG.height, 0xFFC24B99));
-				add(buttonDown = createHint(160, 0, Std.int(FlxG.width / 8), FlxG.height, 0xFF00FFFF));
-				add(button5 = createHint(320, 0, Std.int(FlxG.width / 8), FlxG.height, 0xFFC24B99));
-				add(button6 = createHint(480, 0, Std.int(FlxG.width / 8), FlxG.height, 0xFF00FFFF));
-				add(button7 = createHint(640, 0, Std.int(FlxG.width / 8), FlxG.height, 0xFF12FA05));
-				add(button8 = createHint(800, 0, Std.int(FlxG.width / 8), FlxG.height, 0xFFF9393F));
-				add(buttonUp = createHint(960, 0, Std.int(FlxG.width / 8), FlxG.height, 0xFF12FA05));
-				add(buttonRight = createHint(1120, 0, Std.int(FlxG.width / 8), FlxG.height, 0xFFF9393F));
-			case NINE:
-				add(buttonLeft = createHint(0, 0, Std.int(FlxG.width / 9), FlxG.height, 0xFFC24B99));
-				add(buttonDown = createHint(142, 0, Std.int(FlxG.width / 9), FlxG.height, 0xFF00FFFF));
-				add(button5 = createHint(284, 0, Std.int(FlxG.width / 9), FlxG.height, 0xFFC24B99));
-				add(button6 = createHint(426, 0, Std.int(FlxG.width / 9), FlxG.height, 0xFF00FFFF));
-				add(button7 = createHint(568, 0, Std.int(FlxG.width / 9), FlxG.height, 0xFF12FA05));
-				add(button8 = createHint(710, 0, Std.int(FlxG.width / 9), FlxG.height, 0xFFF9393F));
-				add(button9 = createHint(852, 0, Std.int(FlxG.width / 9), FlxG.height, 0xFF12FA05));
-				add(buttonUp = createHint(994, 0, Std.int(FlxG.width / 9), FlxG.height, 0xFF12FA05));
-				add(buttonRight = createHint(1136, 0, Std.int(FlxG.width / 9), FlxG.height, 0xFFF9393F));
 		}
 
 		for (button in Reflect.fields(this))
@@ -221,4 +166,4 @@ class FlxHitbox extends FlxMobileInputManager
 		bitmap.draw(shape);
 		return bitmap;
 	}
-}
+		}
