8c4faf39c5972ead3d7b3545d473dfc0f0509e0f
Hero <78842166+Heropowerbrine@users.noreply.github.com>
2024-05-23 08:47:54 +0800

    Update Controls.hx

--- source/backend/Controls.hx
+++ source/backend/Controls.hx
@@ -398,7 +398,6 @@ class Controls
 		return result
 			|| _myGamepadJustPressed(gamepadBinds[key]) == true
 			|| mobileCJustPressed(mobileBinds[key]) == true
-			|| mobilehitboxJustPressed(mobileBinds[key]) == true
 			|| virtualPadJustPressed(mobileBinds[key]) == true;
 	}
 
@@ -410,7 +409,6 @@ class Controls
 		return result
 			|| _myGamepadPressed(gamepadBinds[key]) == true
 			|| mobileCPressed(mobileBinds[key]) == true
-			|| mobilehitboxPressed(mobileBinds[key]) == true
 			|| virtualPadPressed(mobileBinds[key]) == true;
 	}
 
@@ -422,7 +420,6 @@ class Controls
 		return result
 			|| _myGamepadJustReleased(gamepadBinds[key]) == true
 			|| mobileCJustReleased(mobileBinds[key]) == true
-			|| mobilehitboxJustReleased(mobileBinds[key]) == true
 			|| virtualPadJustReleased(mobileBinds[key]) == true;
 	}
 
@@ -546,45 +543,6 @@ class Controls
 	private function mobileCJustReleased(keys:Array<FlxMobileInputID>):Bool
 	{
 		if (keys != null && requested.mobileControls != null && gameplayRequest != null)
-		{
-			if (gameplayRequest.anyJustReleased(keys))
-			{
-				controllerMode = true;
-				return true;
-			}
-		}
-		return false;
-	}
-
-	private function mobilehitboxPressed(keys:Array<FlxMobileInputID>):Bool
-	{
-		if (keys != null && requested.hitbox != null && gameplayRequest != null)
-		{
-			if (gameplayRequest.anyPressed(keys))
-			{
-				controllerMode = true;
-				return true;
-			}
-		}
-		return false;
-	}
-
-	private function mobilehitboxJustPressed(keys:Array<FlxMobileInputID>):Bool
-	{
-		if (keys != null && requested.hitbox != null && gameplayRequest != null)
-		{
-			if (gameplayRequest.anyJustPressed(keys))
-			{
-				controllerMode = true;
-				return true;
-			}
-		}
-		return false;
-	}
-
-	private function mobilehitboxJustReleased(keys:Array<FlxMobileInputID>):Bool
-	{
-		if (keys != null && requested.hitbox != null && gameplayRequest != null)
 		{
 			if (gameplayRequest.anyJustReleased(keys))
 			{
