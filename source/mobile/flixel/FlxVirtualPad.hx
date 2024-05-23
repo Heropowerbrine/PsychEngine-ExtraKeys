f410a98fe3e15b80315cb447588380c64d02606b
Hero <78842166+Heropowerbrine@users.noreply.github.com>
2024-05-23 16:59:50 +0800

    Update FlxVirtualPad.hx

--- source/mobile/flixel/FlxVirtualPad.hx
+++ source/mobile/flixel/FlxVirtualPad.hx
@@ -76,7 +76,7 @@ class FlxVirtualPad extends FlxMobileInputManager {
 
 		if (DPad != "NONE") {
 			if (!MobileData.dpadModes.exists(DPad))
-				throw 'The virtualPad dpadMode "$DPad" doesn\'nt exists.';
+				throw 'The virtualPad dpadMode "$DPad" doesn\'t exists.';
 			for (buttonData in MobileData.dpadModes.get(DPad).buttons) {
 				Reflect.setField(this, buttonData.button,
 					createButton(buttonData.x, buttonData.y, buttonData.width, buttonData.height, buttonData.graphic,
@@ -87,7 +87,7 @@ class FlxVirtualPad extends FlxMobileInputManager {
 
 		if (Action != "NONE") {
 			if (!MobileData.actionModes.exists(Action))
-				throw 'The virtualPad actionMode "$Action" doesn\'nt exists.';
+				throw 'The virtualPad actionMode "$Action" doesn\'t exists.';
 			for (buttonData in MobileData.actionModes.get(Action).buttons) {
 				Reflect.setField(this, buttonData.button,
 					createButton(buttonData.x, buttonData.y, buttonData.width, buttonData.height, buttonData.graphic,
@@ -98,11 +98,11 @@ class FlxVirtualPad extends FlxMobileInputManager {
 
 		switch (Extra) {
 			case SINGLE:
-				add(buttonExtra = createButton(0, FlxG.height - 135, 132, 127, 's', 0xFF0066FF));
+				add(buttonExtra = createButton(0, FlxG.height - 137, 132, 125, 's', 0xFF0066FF));
 				setExtrasPos();
 			case DOUBLE:
-				add(buttonExtra = createButton(0, FlxG.height - 135, 132, 127, 's', 0xFF0066FF));
-				add(buttonExtra2 = createButton(FlxG.width - 132, FlxG.height - 135, 132, 127, 'g', 0xA6FF00));
+				add(buttonExtra = createButton(0, FlxG.height - 137, 132, 125, 's', 0xFF0066FF));
+				add(buttonExtra2 = createButton(FlxG.width - 132, FlxG.height - 137, 132, 125, 'g', 0xA6FF00));
 				setExtrasPos();
 			case NONE: // nothing
 		}
@@ -111,43 +111,71 @@ class FlxVirtualPad extends FlxMobileInputManager {
 		updateTrackedButtons();
 	}
 
+	override function update(elapsed:Float)
+	{
+		forEachAlive((button:FlxButton) -> {
+			if(!button.isOnScreen(button.camera))
+			{
+				if(button.x < 0)
+					button.x = 0;
+				if(button.y < 0)
+					button.y = 0;
+				if(button.x > FlxG.width - button.frameWidth)
+					button.x = FlxG.width - button.frameWidth;
+				if(button.y > FlxG.height - button.frameHeight)
+					button.y = FlxG.height - button.frameHeight;
+			}
+		});
+		super.update(elapsed);
+	}
+
+	override public function destroy() {
+		super.destroy();
+
+		for (field in Reflect.fields(this))
+			if (Std.isOfType(Reflect.field(this, field), FlxButton))
+				Reflect.setField(this, field, FlxDestroyUtil.destroy(Reflect.field(this, field)));
+	}
+
 	public function setExtrasDefaultPos() {
 		var int:Int = 0;
 
-		if (FlxG.save.data.extraData == null)
-			FlxG.save.data.extraData = new Array();
+		if (MobileControls.save.data.extraData == null)
+			MobileControls.save.data.extraData = new Array();
 
 		for (button in Reflect.fields(this)) {
 			if (button.toLowerCase().contains('extra') && Std.isOfType(Reflect.field(this, button), FlxButton)) {
 				var daButton = Reflect.field(this, button);
-				if (FlxG.save.data.extraData[int] == null)
-					FlxG.save.data.extraData.push(FlxPoint.get(daButton.x, daButton.y));
+				if (MobileControls.save.data.extraData[int] == null)
+					MobileControls.save.data.extraData.push(FlxPoint.get(daButton.x, daButton.y));
 				else
-					FlxG.save.data.extraData[int] = FlxPoint.get(daButton.x, daButton.y);
+					MobileControls.save.data.extraData[int] = FlxPoint.get(daButton.x, daButton.y);
 				++int;
 			}
 		}
-		FlxG.save.flush();
+		MobileControls.save.flush();
 	}
 
 	public function setExtrasPos() {
 		var int:Int = 0;
-		if (FlxG.save.data.extraData == null)
+		if (MobileControls.save.data.extraData == null)
 			return;
 
 		for (button in Reflect.fields(this)) {
 			if (button.toLowerCase().contains('extra') && Std.isOfType(Reflect.field(this, button), FlxButton)) {
 				var daButton = Reflect.field(this, button);
-				daButton.x = FlxG.save.data.extraData[int].x;
-				daButton.y = FlxG.save.data.extraData[int].y;
+				daButton.x = MobileControls.save.data.extraData[int].x;
+				daButton.y = MobileControls.save.data.extraData[int].y;
 				int++;
 			}
 		}
 	}
 
 	private function createButton(X:Float, Y:Float, Width:Int, Height:Int, Graphic:String, ?Color:Int = 0xFFFFFF, ?IDs:Array<FlxMobileInputID>):FlxButton {
-		var button = new FlxButton(X, Y);
+		var button = new FlxButton(X, Y, IDs);
 		button.frames = FlxTileFrames.fromFrame(getSparrowAtlas('mobile/images/virtualpad').getByName(Graphic), FlxPoint.get(Width, Height));
+		button.bounds.makeGraphic(Std.int(button.frameWidth - 50), Std.int(button.frameHeight - 50), FlxColor.TRANSPARENT);
+		button.centerBounds();
 		button.resetSizeFromFrame();
 		button.solid = false;
 		button.immovable = true;
@@ -156,26 +184,18 @@ class FlxVirtualPad extends FlxMobileInputManager {
 		button.color = Color;
 		button.antialiasing = ClientPrefs.data.antialiasing;
 		button.tag = Graphic.toUpperCase();
-		button.IDs = IDs;
 		#if FLX_DEBUG
 		button.ignoreDrawDebug = true;
 		#end
 		return button;
 	}
 
-	override public function destroy():Void {
-		super.destroy();
-
-		for (field in Reflect.fields(this))
-			if (Std.isOfType(Reflect.field(this, field), FlxButton))
-				Reflect.setField(this, field, FlxDestroyUtil.destroy(Reflect.field(this, field)));
-	}
-
 	function getSparrowAtlas(key:String):FlxAtlasFrames {
 		var file:String = '';
 		var bitmap:BitmapData = null;
 		#if MODS_ALLOWED
-		file = Paths.modsImages(key);
+		if(Mods.currentModDirectory != null && Mods.currentModDirectory.length > 0)
+			file = haxe.io.Path.join([Mods.currentModDirectory, key]);
 		if (Paths.currentTrackedAssets.exists(file)) {
 			Paths.localTrackedAssets.push(file);
 			bitmap = Paths.currentTrackedAssets.get(file).bitmap;
@@ -205,4 +225,4 @@ class FlxVirtualPad extends FlxMobileInputManager {
 		return FlxAtlasFrames.fromSparrow(imageLoaded, Paths.getPath('$key.xml'));
 		#end
 	}
-	}
+		}
